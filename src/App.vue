e045db7bb098ea99d9cfc412e936e14705738770
jiangzaihao <1204628253@qq.com>
2025-12-16 22:50:26 +0800

    not show Appupload page

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,43 @@
-<script >
-export default  {
-    name:"APP",
-}
-</script>
+
 
 <template>
- 
-</template>
+ <van-nav-bar title="维修工单系统" />
+ <van-tabs >
+      <van-tab title="首页"></van-tab>
+  <van-tab title="AdminLogin管理员登陆"></van-tab>
+  <van-tab title="queryissue查询工单"></van-tab>
+  <van-tab title="history历史工单"></van-tab>
+</van-tabs>
+<component :is="componentName"></component>
 
-<style setup>
+</template>
+<script >
+    import APPUpload from "@/components/Upload.vue";
+import {ref} from "vue";
+export default  {
+    name:"APP",
+    setup()
+    {
+        const poster=ref("");
+        const description=ref("");
+        const componentName=ref("APPUpload")
+        return
+        {
+            poster,
+            description,
+            componentName
+        };
+
+    },
+    components:
+    {APPUpload,
+
+    }
+}
+
+
+</script>
+<style >
 
 
 </style>
---
+++ src/components/Upload.vue
@@ -0,0 +1,37 @@
+<template>
+<van-form @submit="onSubmit">
+  <van-cell-group inset>
+    <van-field
+      v-model="poster"
+      name="提交人"
+      label="提交人"
+      placeholder="提交人"
+      :rules="[{ required: true, message: '请填写提交人' }]"
+    />
+    <van-field
+      v-model="description"
+      name="工单描述"
+      label="工单描述"
+      placeholder="工单描述"
+      :rules="[{ required: true, message: '请填写工单描述' }]"
+    />
+  </van-cell-group>
+  <div style="margin: 16px;">
+    <van-button round block type="primary" native-type="submit">
+      提交
+    </van-button>
+  </div>
+</van-form>
+
+</template>
+<script>
+    export default
+    {
+        name:"APPUpload",
+    }
+    
+
+</script>
+<style>
+
+</style>
