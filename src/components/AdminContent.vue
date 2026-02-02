3a76af1a4e3589b85b9f87af1e2324a62ff15874
jiangzaihao <1204628253@qq.com>
2026-02-02 20:56:04 +0800

    finish AppAdminIssueList

--- src/components/AdminContent.vue
+++ src/components/AdminContent.vue
@@ -1,19 +1,27 @@
 <template>
     <van-tabs v-model:active="componentName">
-  <van-tab title="工单列表" name="issueList">工单列表</van-tab>
-  <van-tab title="创建新员工" name="createStaff">创建新员工</van-tab>
-<component :is="componentName"></component>
+  <van-tab title="工单列表" name="AppAdminList">工单列表</van-tab>
+  <van-tab title="创建新员工" name="AppAdminCreateStaff">创建新员工</van-tab>
+
+<!-- <keep-alive>
+  <component :is="componentName"></component>
+</keep-alive> -->
+<app-admin-issue-list></app-admin-issue-list>
 </van-tabs>
 
     </template>
 <script >
 import {ref}from "vue";
 import AppAdminIssueList from "./AdminIssueList.vue";
-const componentName=ref("AppAdminIssueList")
 export default
 {
-  name:{AdminContent},
-  component:
+  setup(){
+const componentName=ref("AppAdminIssueList");
+    return {componentName};
+
+  },
+  name:"AppAdminContent",
+  components:
   {
     AppAdminIssueList,
   }
--- src/components/AdminIssueList.vue
+++ src/components/AdminIssueList.vue
@@ -3,9 +3,16 @@
     <h3 v-if="issueObjArr.length>0">历史工单数量{{ issueObjArr.length   }}
       <div v-for="issueObj in issueObjArr">
           <van-list>
-  <van-cell v-for="item in processIssueObj(issueObj)" :key="item" :title="item" />
+  <van-cell v-for="item in processIssueObj(issueObj)" :key="item" :title="item" /><van-divider />
 </van-list>
-<div class="btn-margin" ><van-button round block type="primary" @click.prevent="showImages(issueObj)" >查看图片</van-button></div>
+<div class="btn-margin" ><van-button round block type="primary" @click.prevent="showImages(issueObj)" >查看图片</van-button>
+<br>
+
+    <van-button round block type="success" v-if="issueObj.state=='wait'" >分配工单号</van-button>
+</div>
+<van-divider />
+
+
       </div>
          
     </h3>
@@ -15,23 +22,31 @@
 
 <script setup>
 import Config from "../assets/js/config.js"
+   import {onMounted} from "vue";
 import {ref} from"vue";
     import { showImagePreview } from 'vant';
 import { processIssueObj } from "@/assets/js/util.js";
 const {API_URL} =Config;
 const issueObjArr=ref("");
+const showImages=(issueObj)=>
+{
+  showImagePreview({
+  images: JSON.parse(issueObj.image),
+  closeable: true,
+});
+}
 const loadIssueList=async()=>{
  const response=await fetch(`${API_URL}issue`);
-    const result=response.json();
+    const result= await response.json();
     if(!response.ok||result.status!=="success")
 {
     alert("加载失败");
     return ;
 }
-    issueObjArr.value=JSON.parse(result.data);
+    issueObjArr.value=(result.data);
 }
 onMounted(async() => {
-    
+    loadIssueList();
 })
 </script>
 
