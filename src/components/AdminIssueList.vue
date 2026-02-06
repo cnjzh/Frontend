d0e1cb0b2b37bd7f0bd0a9a4d283a2c333189a01
jiangzaihao <1204628253@qq.com>
2026-02-06 22:51:17 +0800

    backend updata Issue has problems

--- src/components/AdminIssueList.vue
+++ src/components/AdminIssueList.vue
@@ -1,15 +1,8 @@
 <template>
-    <div v-show="show_staff_list">
- <van-list>
-  <van-cell 
-  v-for="staff in staffArr" 
-  :key="staff" 
-  :title="staff" />
   
-  <van-divider />
-</van-list>
-    </div>
-<div  class="center">
+
+  
+<div  class="center"  v-show="!show_staff_list">
     <h3 v-if="issueObjArr.length>0">历史工单数量{{ issueObjArr.length   }}
       <div v-for="issueObj in issueObjArr">
           <van-list>
@@ -21,7 +14,7 @@
     <!-- <van-button round block type="success"  @click.prevent="assignStaff" >分配负责人</van-button> -->
     
 
-    <van-button type="success" round block @click.prevent="show_staff_list=true"  v-if="issueObj.state=='wait'">分配负责人</van-button>
+    <van-button type="success" round block @click.prevent="goToAssign(issueObj.id)"  v-if="issueObj.state=='wait'">分配负责人</van-button>
 
 
 
@@ -34,6 +27,29 @@
     </h3>
     <h3 v-else class="center">未找到历史工单</h3>
 </div>
+
+ <van-list v-show="show_staff_list">
+    <div v-for="staff in staffArr" :key="staff.staffName"  class="btn-margin" >
+        <van-cell 
+         v-if="staff.staffRole=='staff'"
+         v-for="item in processStaffObj(staff)"
+        :title="item"  
+        :key="item"
+        
+       />
+        <!-- <van-cell  v-for="item in Object.values(staff)":title="item"  /> -->
+         <van-button 
+         type="success" 
+         round block 
+         @click.prevent="assignStaff(staff.id)"  
+        v-if="staff.staffRole=='staff'"
+         class="btn-margin" >
+         选择 负责人
+        </van-button> 
+        <van-divider />
+    </div>
+   
+</van-list>
 </template>
 
 <script setup>
@@ -46,7 +62,7 @@ const show_staff_list=ref(false);
 
 
 const {API_URL} =Config;
-const issueObjArr=ref("");
+const issueObjArr=ref([]);
 const showPopover=ref("");
 const staffArr=ref([]);
 const showImages=(issueObj)=>
@@ -78,18 +94,31 @@ const loadStaffList=async()=>{
     alert("加载员工信息失败");
     return ;
 }
-    staffArr.value=result.data.map((staff)=>{
-        return    `${staff.id}:${staff.staffName}`
-    } );
+    staffArr.value=result.data;
 }
-const assignStaff=(action)=>
+const goToAssign=(id)=>
 {
-    showToast(action.text);
+    assignStaffId.value=id;
+    show_staff_list.value = true;
+}
+//TODO backend updata Issue has problems
+const assignStaff= async (id)=>{
+    fetch(`${API_URL}issue/`)
 }
 onMounted(async() => {
     loadIssueList();
     loadStaffList();
 })
+const assignIssueId=ref(0);
+const assignStaffId=ref(0);
+const processStaffObj=(staff)=>
+{
+    const res=[];
+    res.push(`员工号:${staff.id}`);
+    res.push(`员工:${staff.staffName}`);
+    return res;
+
+}
 </script>
 
 <style >
